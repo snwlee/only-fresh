03412beeab0f526495867bbb22f80b139d7a0f28
dr94406 <dr94406@naver.com>
2022-08-29 15:58:01 +0900

    ⚙️ chore : JS 분리, 자잘한 수정

--- src/main/webapp/WEB-INF/views/product/productNewlist.jsp
+++ src/main/webapp/WEB-INF/views/product/productNewlist.jsp
@@ -72,7 +72,8 @@
         }
     </style>
     <script src="https://code.jquery.com/jquery-1.11.3.js"></script>
-    <script src="/product/product.js"></script>
+    <!--<script src="/product/product.js"></script>-->
+    <script type="text/javascript" src="/category/js/category.js"></script>
 </head>
 <body>
 <div id="whole_container">
@@ -196,42 +197,6 @@
 </div>
 
 <!--<script>
-    <%--카테고리 --%>
-    let wrapper = $("#cat_wrapper");
-    let show_category_button = $("#show_category_button");
-    let main_cat_container = $("#main_cat_container");
-    let sub_cat_container = $("#sub_cat_container");
-    let sub_cat = $(".sub_cat");
-
-    show_category_button.hover(() => {
-        main_cat_container.show();
-    })
-
-    wrapper.mouseleave(() => {
-        main_cat_container.hide();
-        sub_cat_container.hide();
-    })
-
-    sub_cat_container.mouseleave(() => {
-        sub_cat_container.hide();
-    })
-
-    let categories = null;
-
-    let catToLi = function (res) {
-        let tmp = '';
-
-        res.forEach(el => {
-            tmp += '<a href="/product/newlist?cd_name_num='
-            tmp += el.cd_name_num
-            tmp += '&page=1&pageSize=12"<li class="cat main_cat">'
-            tmp += el.cd_name
-            tmp += '</li></a>'
-        })
-
-        return tmp;
-    }
-
     let page = '<c:out value="${param.page}"/>';
     let pageSize = '<c:out value="${param.pageSize}"/>';
     let sort = '<c:out value="${param.sort}"/>';
@@ -275,34 +240,6 @@
 
     $(document).ready(function () {
         showList();
-        $.ajax({
-            type: 'GET',       // 요청 메서드
-            url: '/product/categories',  // 요청 URI
-            success: function (res) {
-                categories = res;
-                $.each(res, (el) => {
-                    $("#main_cat_container").append('<a href="/product/newlist?cd_type_name=' + el + '&page=1&pageSize=12&order_sc=in_date&asc=sel_price%20ASC"<li class="cat main_cat">' + el + '</li></a>');
-                })
-            },
-            error: function (result) {
-                alert("쿠폰 불러오기 실패");
-            }, // 에러가 발생했을 때, 호출될 함수
-            complete: function () {
-                $(".main_cat").mouseenter((e) => {
-                    sub_cat_container.show();
-                    sub_cat_container.html(catToLi(categories[e.currentTarget.innerText]));
-                })
-            }
-        })
-        //검색
-        $("#search_btn").click(function () {
-            let keyword = $("#keyword").val();
-            window.location.href = '/product/newlist?sort=1&keyword=' + keyword + '&page=1&pageSize=12&order_sc=in_date';
-        });
-        $("input[id=keyword]").keydown(function (key) {
-            if (key.keyCode == 13)
-                $("#search_btn").trigger("click");
-        }); //검색 끝
     })
 </script>-->
 </body>
--- src/main/webapp/resources/product/product.js
+++ src/main/webapp/resources/product/product.js
@@ -1,16 +1,17 @@
-    let page = '<c:out value="${param.page}"/>';
-    let pageSize = '<c:out value="${param.pageSize}"/>';
-    let sort = '<c:out value="${param.sort}"/>';
-    let cd_name_num = '<c:out value="${param.cd_name_num}"/>';
-    let cd_type_name = '<c:out value="${param.cd_type_name}"/>';
-    let order_sc = '${param.order_sc}';
-    let asc = '${param.asc}';
-    let keyword = '<c:out value="${ph.sc.keyword}"/>';
+let page = '<c:out value="${param.page}"/>';
+let pageSize = '<c:out value="${param.pageSize}"/>';
+let sort = '<c:out value="${param.sort}"/>';
+let cd_name_num = '<c:out value="${param.cd_name_num}"/>';
+let cd_type_name = '<c:out value="${param.cd_type_name}"/>';
+let order_sc = '${param.order_sc}';
+let asc = '${param.asc}';
+let keyword = '<c:out value="${ph.sc.keyword}"/>';
 
-    let showList = function () {
+let showList = function () {
     $.ajax({
         type: 'GET',
         url: '/product/call?sort=' + sort + '&cd_name_num=' + cd_name_num + '&cd_type_name=' + cd_type_name + '&page=' + page + '&pageSize=' + pageSize + '&order_sc=' + order_sc + '&asc=' + asc + '&keyword=' + keyword,
+        // http://localhost/product/newlist?page=1&pageSize=12&cd_name_num=1&cd_type_name=%27%EC%B1%84%EC%86%8C%27&sort=0
         success: function (result) {
             $("#product").html(toHtml(result.list)); // 상품 리스트를 가져온다.
             $("#cd_type_name").text(result.cd_type_name);
@@ -23,19 +24,22 @@
     });
 }
 
-    let toHtml = function (lists) {
+let toHtml = function (lists) {
     let tmp = "";
     lists.forEach(function (ProductDto) {
-    tmp += '<div class="products">'
-    tmp += '<a href="/detail?pdt_id=' + ProductDto.pdt_id + '"><img id="img" src="' + ProductDto.image + '"/></a>'
-    tmp += '<span class="de_type">' + (ProductDto.de_type == true ? "샛별배송" : "낮배송") + '</span>'
-    tmp += '<div class="product_title">' + ProductDto.title + '</div>'
-    tmp += '<span class="product_ds_rate">' + ProductDto.ds_rate + '%' + '<span class="product_sel_price">' + ProductDto.sel_price.toLocaleString() + '원</span></span>'
-    tmp += '<span class="product_price">' + ProductDto.price.toLocaleString() + '원</span>'
-    tmp += '<span class="product_tag">' + ProductDto.tag_name + '</span></div>'
-})
+        tmp += '<div class="products">'
+        tmp += '<a href="/detail?pdt_id=' + ProductDto.pdt_id + '"><img id="img" src="' + ProductDto.image + '"/></a>'
+        tmp += '<span class="de_type">' + (ProductDto.de_type == true ? "샛별배송" : "낮배송") + '</span>'
+        tmp += '<div class="product_title">' + ProductDto.title + '</div>'
+        tmp += '<span class="product_ds_rate">' + ProductDto.ds_rate + '%' + '<span class="product_sel_price">' + ProductDto.sel_price.toLocaleString() + '원</span></span>'
+        tmp += '<span class="product_price">' + ProductDto.price.toLocaleString() + '원</span>'
+        tmp += '<span class="product_tag">' + ProductDto.tag_name + '</span></div>'
+    })
     return tmp;
 }
-    $(document).ready(function () {
-    showList();})
+
+
+$(document).ready(function () {
+    showList();
+})
 
