037c16a06af435cb3302d1c8710c102d12c80fa0
pgrrr <pgrrr119@gmail.com>
2023-05-13 20:44:34 +0900

    refactor: 주석 처리 코드 제거
    - CartController 주석 처리 코드 제거

--- src/main/java/com/devkurly/cart/controller/CartController.java
+++ src/main/java/com/devkurly/cart/controller/CartController.java
@@ -2,20 +2,15 @@ package com.devkurly.cart.controller;
 
 import com.devkurly.cart.domain.Cart;
 import com.devkurly.cart.dto.CartSaveRequestDto;
-import com.devkurly.cart.exception.MaxCartException;
 import com.devkurly.cart.service.CartService;
-import com.devkurly.global.ErrorCode;
 import com.devkurly.member.dto.MemberMainResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -37,18 +32,10 @@ public class CartController {
         }
         int id = getId(tempCart, response, session);
         requestDto.saveCart(id, pdt_id);
-//        cartService.checkAddProductStock(requestDto.toEntity());
         cartService.checkProductStock(requestDto.toEntity());
         cartService.addCart(requestDto);
         return "redirect:/carts";
     }
-
-//    @GetMapping("/delete")
-//    public String removeAllCart(@CookieValue(value = "tempCart", required = false) Cookie tempCart, HttpServletResponse response, HttpSession session) {
-//        int id = getId(tempCart, response, session);
-//        cartService.removeCart(id);
-//        return "redirect:/carts";
-//    }
 
     @GetMapping("/delete/{ptd_id}")
     public String removeOneCart(@PathVariable Integer ptd_id, @CookieValue(value = "tempCart", required = false) Cookie tempCart, HttpServletResponse response, HttpSession session) {
